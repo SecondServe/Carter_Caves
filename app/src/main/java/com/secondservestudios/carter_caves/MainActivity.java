1662254bb4b3e6e5fae086c441462ca6597dbe5b
Jeremiah Lewis <jeremiahlewis0710@gmail.com>
2017-08-29 13:38:19 -0400

    added gps code and comments to manifest and main activity

--- app/src/main/java/com/secondservestudios/carter_caves/MainActivity.java
+++ app/src/main/java/com/secondservestudios/carter_caves/MainActivity.java
@@ -1,13 +1,77 @@
 package com.secondservestudios.carter_caves;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+    //saving the coordinates for the check, double is always the accepted data type.
+    double longitude;
+    double latitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getCoordinates();
+
+    }
+//LocationListener is an active check for when the coordinates change
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private void getCoordinates() {
+        //This deliberately checks to see if the user will grant the app permission to use the gps, if they do not then the function will not continue to run, if left unchecked it could crash the app, Jeremiah will test
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+    }
+
+    private void useCoordinates(){
+        //if(latitude == set latitude && longitude == set longitude){
+        // send push notification to user phone about the site they are on.
+        //This is going to require official descriptions and photos from Carter Caves, Will need their approval before continuing.
+        //Will also need to make the check for within a certain range, specific coordinates will be too picky to be user friendly
+        // }
+
     }
 }
