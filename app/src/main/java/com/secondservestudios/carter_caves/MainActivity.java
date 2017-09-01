ad40718d38900eca73600b5fd4a1e8fbf05d0670
jeremiahlewis <jeremiahlewis0710@gmail.com>
2017-09-01 19:59:54 -0400

    Commenting tasks

--- app/src/main/java/com/secondservestudios/carter_caves/MainActivity.java
+++ app/src/main/java/com/secondservestudios/carter_caves/MainActivity.java
@@ -4,10 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    // The GPS permissions have a line in the android Manifest
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Begin gps coordinates method
+        // nofityUserMethod();
+
     }
 }
